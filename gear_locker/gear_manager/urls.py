d1da2d8def47f21beb02598098f1134b06594e58
Pat McGhen <patrickmcghen@gmail.com>
2024-01-25 09:25:07 -0500

    Add login form

--- gear_locker/gear_manager/urls.py
+++ gear_locker/gear_manager/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login_user, name='login'),
     path('gear/add', views.add_gear, name='add_gear'),
 ]
--- gear_locker/gear_manager/views.py
+++ gear_locker/gear_manager/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
 
 def index(request):
     return render(request, 'gear_manager/index.html')
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return render(request, 'gear_manager/index.html')
+        else:
+            return render(request, 'gear_manager/login.html', {'error_message': 'Invalid login'})
+    else:
+        return render(request, 'gear_manager/login.html')
 
 
 def add_gear(request):
